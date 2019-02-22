4c9ac05195171f7ce2860184c3dace98dafa658f
Montsheey <47817520+Montsheey@users.noreply.github.com>
2019-02-22 14:51:44 +0200

    Add files via upload

---
+++ VioletLoti.js
@@ -0,0 +1,67 @@
+// challenge1
+
+var a = (num);
+var b = (num);
+
+function name() {
+if (a == 65 || b == 65) {
+  return true
+}
+else if (a + b == 65) {
+  return true
+}
+else {
+  return false;
+}
+}
+console.log(name());
+
+//challenge 2
+
+var side1 = 7;
+var side2 = 8;
+var side3 = 9;
+var s = (side1 + side2 + side3)/2;
+var area = Math.sqrt (s* (s-side1) * (s-side2) * (s-side3));
+console.log (area)
+
+// challenge 3
+
+function largest(num1, num2, num3) {
+  var num1 = num1;
+  var num2 = num2;
+  var num3 = num3;
+
+  if (num1 > num2);
+  return largest(num)
+}
+elseif (num2 > num3); {
+  return largest(num)
+}
+if (num3 > num1); {
+  return largest(num)
+}
+console.log largest(num)
+
+// challenge 4
+
+function timeConverter(num) {
+  var hours = Math.floor (num / 60);
+  var minutes = (num % 60);
+  return hours + ":" + minutes
+}
+
+// challenge 5
+
+var one = string1;
+var two = string2;
+var commonCharacters;
+function commonCharacters (string1,string2) {
+   var one = string1.replace(/ /g "");
+   var two = string2.replace(/ /g. "");
+   for (var i = 0; 1 < one.length; i++) {
+     if(two. indexOf(one[1])) ===- -1 && result.
+     result.push(one[i]) }
+
+}
+console.log commonCharacters("");
